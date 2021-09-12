ef48da77fd65e825bb3069109d106335242b0493
Anuj SP <59767605+anuj-padhiyar@users.noreply.github.com>
2021-09-12 11:50:47 +0530

    Completed with optimization

--- Find S Algorithm/FSalgo.py
+++ Find S Algorithm/FSalgo.py
@@ -4,16 +4,17 @@ with open('test2.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
     table = list(csvReader)
     header = table.pop(0)
-    for row in table:
-        if row.pop(-1)=="yes":
-            if "hypothesis" in locals():
-                for i in range(len(row)):
-                    if hypothesis[i] != row[i]:
-                        hypothesis[i] = '?'
-            else:
-                hypothesis = row.copy()
 
-    if "hypothesis" not in locals():
-        print("No Data Availble")
-    else:
-        print("Hypothesis Is ",hypothesis)
+for row in table:
+    if row.pop(-1)=="yes":
+        if "hypothesis" in locals():
+            for i in range(len(row)):
+                if hypothesis[i] != row[i]:
+                    hypothesis[i] = '?'
+        else:
+            hypothesis = row.copy()
+
+if "hypothesis" not in locals():
+    print("No Data Availble")
+else:
+    print("Hypothesis Is ",hypothesis)
